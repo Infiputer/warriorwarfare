b6488c8d8c490a4a506afbdfdda8957431dd67f4
Infiputer <88679343+Infiputer@users.noreply.github.com>
2024-07-19 16:41:18 -0700

    Format homepage.js

--- homepage.js
+++ homepage.js
@@ -1,24 +1,24 @@
-	tips = [
-		"WASD to move",
-		"Click to shoot",
-		"Bounce on trampolines",
-		"Hide in a castle or a house",
-		"Made by Infiputer"
-	]
-	tipnum = Math.floor(Math.random()*tips.length);
-	function showTip(){
-		document.getElementById("tips").innerHTML = tips[tipnum];
-		tipnum++;
-		tipnum=tipnum%tips.length;
-	}
-	function changeTip(){
-		for(deg = 0; deg<360;deg+=30){
-			setTimeout(function(x){
-				console.log(x)
-				document.getElementById("tips").style.transform="rotateX("+x+"deg)";
-			}, deg*10, deg)
-		}
-		setTimeout(showTip, 700);
-	}
-	changeTip();
-	setInterval(changeTip, 5000);
+tips = [
+    "WASD to move",
+    "Click to shoot",
+    "Bounce on trampolines",
+    "Hide in a castle or a house",
+    "Made by Infiputer"
+]
+tipnum = Math.floor(Math.random() * tips.length);
+function showTip() {
+    document.getElementById("tips").innerHTML = tips[tipnum];
+    tipnum++;
+    tipnum = tipnum % tips.length;
+}
+function changeTip() {
+    for (deg = 0; deg < 360; deg += 30) {
+        setTimeout(function (x) {
+            console.log(x)
+            document.getElementById("tips").style.transform = "rotateX(" + x + "deg)";
+        }, deg * 10, deg)
+    }
+    setTimeout(showTip, 700);
+}
+changeTip();
+setInterval(changeTip, 5000);
