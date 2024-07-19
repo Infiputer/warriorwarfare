bff6800353f2870377fb1482da0847a3c2c857ff
Infiputer <88679343+Infiputer@users.noreply.github.com>
2024-07-19 16:40:43 -0700

    Format soundeffects.js

--- soundeffects.js
+++ soundeffects.js
@@ -1,6 +1,6 @@
 function playSound(filename, x, z) {
-	soundVolume = mathmap(distance2d(myX, myZ, x, z), 0, 10, 1, 0);
-  soundeffectAudio = new Audio('/assets/sounds/' + filename);
-	soundeffectAudio.volume = soundVolume;
-  soundeffectAudio.play();
+    soundVolume = mathmap(distance2d(myX, myZ, x, z), 0, 10, 1, 0);
+    soundeffectAudio = new Audio('/assets/sounds/' + filename);
+    soundeffectAudio.volume = soundVolume;
+    soundeffectAudio.play();
 }
